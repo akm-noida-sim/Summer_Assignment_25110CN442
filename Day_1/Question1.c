26c93b0e7ee3cbf66970d754bb521eab722bdac1
akm-noida-sim <akm.aljn.noida@gmail.com>
2026-06-02 20:00:31 +0530

    change

--- Day_1/Question1.c
+++ Day_1/Question1.c
@@ -2,17 +2,20 @@
 int main()
 {
     int num, sum=0;                                                         //variable initialisation
-
+    
     printf("Enter the number till you want to find the sum: ");            //Input
-
+    
     scanf("%d", &num);                                                    // storing the variable in the declared "num"
-
+    
     for(int i=1 ; i<=num ; i++)                                             // loop for the result (for loop)
+    
     {
+    
         sum = sum + i;                                                    // statement block
+    
     }
-
+    
     printf("The number of starting %d natural number is %d: ", num,sum);   //printing the output
-
+    
     return 0;
 }
